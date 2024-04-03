2b222dfda24e81a3f03548004b195b02d8afcc41
cesarqui2021 <160754674+cesarqui2021@users.noreply.github.com>
2024-04-03 09:12:46 -0600

    Add files via upload

---
+++ Tarea-3-de-abril.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int num1 = 0;
+	cout << "Por favor ingrese un numero deñ 1 al 9 para determinar si es area comun o especifica o si no estamos en ningun semestre"<<endl;
+	cin >> num1;
+	
+	switch(num1) {
+	case 1:
+		cout << "Area comun"<<endl;
+		break;
+	case 2:
+		cout << "No pertenece a ningun semestre"<<endl;
+		break;
+	case 3:
+		cout << "Area comun"<<endl;
+		break;	
+	case 4:
+		cout << "No pertenece a ningun semestre"<<endl;
+		break;	
+	case 5:
+		cout << "Area especifica"<<endl;
+		break;	
+	case 6:
+		cout << "No pertenece a ningun semestre"<<endl;
+		break;	
+	case 7:
+		cout << "Area especifica"<<endl;
+		break;	
+	case 8:
+		cout << "No pertenece a ningun semestre"<<endl;
+		break;	
+	case 9:
+		cout << "No pertenece a ningun semestre"<<endl;
+		break;	
+	}
+	return 0;
+}
+
