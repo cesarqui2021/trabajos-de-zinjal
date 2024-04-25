ab0086d72919e0ee57474c214f116919aa98197b
cesarqui2021 <160754674+cesarqui2021@users.noreply.github.com>
2024-04-24 22:24:48 -0600

    Add files via upload

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author jhonm
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+ System.out.println("Ejemplos de las Variables");
+         String cadena = "Hola";
+        Integer entero1 = 5;
+        Integer entero2 = 10;
+        Double decimal1 = 3.1416;
+        Double decimal2 = 3.1416;
+        Boolean booleano = true;
+        String mensaje = cadena + " amigos de la universidad";
+        System.out.println(mensaje);
+        Integer sumaEnteros = entero1 + entero2;
+        System.out.println("La suma de " + entero1 + " y " + entero2 + " es: " + sumaEnteros);
+      
+         Double numero = 3.14; 
+
+    String texto = "El numero decimal es: "; 
+    System.out.println(texto + numero);   
+
+    Integer numerop = numero.intValue(); 
+    String textos = "El numero en entero es: "; 
+    System.out.println(textos + numerop);  
+
+    Double numero1 = 2.8689; 
+
+    String texto1 = "El numero decimal es: "; 
+    System.out.println(texto1 + numero1);   
+
+    Integer numerop1 = numero1.intValue(); 
+    String textos1 = "El numero en entero es: "; 
+    System.out.println(textos1 + numerop1);  
+
+    Double numero2 = 5.85; 
+
+    String texto2 = "El numero decimal es: "; 
+    System.out.println(texto2 + numero2);   
+
+    Integer numerop2 = numero2.intValue(); 
+    String textos2 = "El numero en entero es: "; 
+    System.out.println(textos2 + numerop2);  
+
+    Double numero4 = 4.49; 
+
+    String texto4 = "El numero decimal es: "; 
+    System.out.println(texto4 + numero4);   
+
+    Integer numerop4 = numero4.intValue(); 
+    String textos4 = "El numero en entero es: "; 
+    System.out.println(textos4 + numerop4);  
+
+    Double numero3 = 2.1; 
+
+    String texto3 = "El numero decimal es: "; 
+    System.out.println(texto3 + numero3);   
+
+    Integer numerop3 = numero3.intValue(); 
+    String textos3 = "El numero en entero es: "; 
+    System.out.println(textos3 + numerop3);  
+
+    //Int a double 
+    Integer numero6 = 1945; 
+
+    String texto6 = "El numero es: "; 
+    System.out.println(texto6 + numero6); 
+
+    String numero_texto6 = numero6.toString(); 
+    String textoo6 = "El numero en decimal es: "; 
+    Double numerooo6 = numero6.doubleValue();  
+    System.out.println(textoo6 + numerooo6); 
+
+    Integer numero7 = 2010; 
+
+    String texto7 = "El numero es: "; 
+    System.out.println(texto7 + numero7); 
+
+    String numero_texto7 = numero7.toString(); 
+    String textoo7 = "El numero en decimal es: "; 
+    Double numerooo7 = numero7.doubleValue();  
+    System.out.println(textoo7 + numerooo7); 
+
+    Integer numero8 = 218; 
+
+    String texto8 = "El numero es: "; 
+    System.out.println(texto8 + numero8); 
+
+    String numero_texto8 = numero8.toString(); 
+    String textoo8 = "El numero en decimal es: "; 
+    Double numerooo8 = numero8.doubleValue();  
+    System.out.println(textoo8 + numerooo8); 
+
+    Integer numero9 = 684; 
+
+    String texto9 = "El numero es: "; 
+    System.out.println(texto9 + numero9); 
+
+    String numero_texto9 = numero9.toString(); 
+    String textoo9 = "El numero en decimal es: "; 
+    Double numerooo9 = numero9.doubleValue();  
+    System.out.println(textoo9 + numerooo9); 
+    }
+}
